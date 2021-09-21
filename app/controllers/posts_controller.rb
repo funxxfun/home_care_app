05375fa8a08e4aa1bae4d667dc63bd4658fa2cb0
funxxfun <23shirasawa@gmail.com>
2021-09-21 01:01:01 +0000

    [Update]post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,30 +3,29 @@ class PostsController < ApplicationController
   def index
     @user = current_user
     @posts = Post.all
+    @patient = Patient.find(params[:patient_id])
     @post = Post.new
     @post.user_id = current_user.id
-    # @patient = Patient.find(params[:patient_id])
   end
 
   def create
     @post = Post.new(post_params)
-    @post.user_id = current_user.id
+    # @post.user_id = current_user.id
+    @patient = Patient.find(params[:id])
     # @post.patient_id = current_patient.id
     @post.save!
-    # redirect_to patient_posts_path(patient)
-    redirect_to posts_path(@post.id)
+    redirect_to patient_posts_path(patient)
+    # redirect_to posts_path(@post.id)
   end
-  
-  
 
 
   def destroy
-    post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     post.destroy
     redirect_to posts_path(post.id)
   end
-  
-  
+
+
   private
 
   def post_params
