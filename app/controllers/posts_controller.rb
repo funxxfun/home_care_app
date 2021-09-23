4b7c0cefb4116f9be5e4f889735862711c0f7c52
funxxfun <23shirasawa@gmail.com>
2021-09-23 11:28:30 +0000

    [Update]post#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,33 +4,21 @@ class PostsController < ApplicationController
     @user = current_user
     @posts = Post.all
     @patient = Patient.find(params[:patient_id])
-    # if params[:id].present?
-    #   @post = Post.find(params[:id])
-    # else
     @post = Post.new
     @post.user_id = current_user.id
-    # @post_edit = Post.find(params[:id])
-    # end
   end
 
   def create
     @post = Post.find_or_initialize_by(content: post_params[:content])
-      # indexページでupdateも行うための記載をしたいがうまくいかない・・・
-    # if @post.new_record?
-      # indexページでupdateも行うための記載をしたいがうまくいかない・・・
-      @post = Post.new(post_params)
-      @post.user_id = current_user.id
-      patient_id = params[:patient_id]
-      @post.patient_id = patient_id
-      @post.save!
-    # else
-      # @post.update(post_params)
-    # end
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    patient_id = params[:patient_id]
+    @post.patient_id = patient_id
+    @post.save!
     redirect_to patient_posts_path(patient_id)
   end
 
 
-# タイムラインedit画面を別に作るならここから↓↓
   def edit
     @post = Post.find(params[:id])
     @patient = Patient.find(params[:patient_id])
@@ -45,8 +33,6 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
-    # @patient = Patient.find(params[:patient_id])
-    # @patient_id = params[:patient_id]
     @post.destroy
     redirect_to patient_posts_path(params[:patient_id])
   end
@@ -55,7 +41,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-      # byebug
     params.require(:post).permit(:image, :content)
   end
 end
