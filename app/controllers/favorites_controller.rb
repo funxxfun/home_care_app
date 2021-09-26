502bdb6f2f0d06895831c28f07640d3122ad96e2
funxxfun <23shirasawa@gmail.com>
2021-09-26 00:54:38 +0000

    [Finish]favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,19 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
-    favorite.save
-    redirect_to patient_posts_path(post)
+    patient = Patient.find(params[:patient_id])
+    favorite = current_user.favorites.new(post_id: post.id, patient_id: patient.id)
+    favorite.save!
+    redirect_to patient_posts_path(patient.id)
   end
 
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: post.id)
     favorite.destroy
-    redirect_to patient_posts_path(post)
+    redirect_to patient_posts_path(post.patient_id, post.id)
   end
-  
+
 end
