0f0505c72671c476378f16e1af5413918b0ccf44
funxxfun <23shirasawa@gmail.com>
2021-10-13 11:49:57 +0000

    Change:graph

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,10 +25,9 @@
 
 // turbolinksの無効化
 $(document).on('turbolinks:load', function() {
-  //(function() {
+  $(function() {
     // .tabがクリックされたときを指定
     $('.tab').click(function(){
-
       // 今ある.tab-activeを削除
       $('.tab-active').removeClass('tab-active');
       // クリックされた.tabに.tab-activeを追加
@@ -37,9 +36,8 @@ $(document).on('turbolinks:load', function() {
       $('.box-show').removeClass('box-show');
       // indexに.tabのindex番号を代入
       const index = $(this).index();
-     
       // .tabboxとindexの番号が同じ要素に.box-showを追加
-      $('.tabbox').eq(index -1 ).addClass('box-show');
+      $('.tabbox').eq(index -1).addClass('box-show');
     });
-  //});
+  });
 });
